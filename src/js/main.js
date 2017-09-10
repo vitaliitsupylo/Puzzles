b0f102035be0094ea6b67bfd06ba23543c76adf7
vitaliitsupylo <vitalii.tsupylo@yandex.ru>
2017-09-10 10:13:23 +0300

    search focus

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,42 @@
-;(()=>{
-'use strict';
-/*Variables*/
+;
+(() => {
+  'use strict';
+  /*Variables*/
 
-/*active list category search*/
-let btnListSearch = document.querySelector('.category_search');
-let activeCatSearch = document.querySelector('.active_category_search');
-let listCatSearch = document.querySelector('.list_category_search');
-let btnCategoryList = document.querySelector('.categories_list');
+  /*active list category search*/
+  const btnListSearch = document.querySelector('.category_search');
+  const activeCatSearch = document.querySelector('.active_category_search');
+  const listCatSearch = document.querySelector('.list_category_search');
+  const btnCategoryList = document.querySelector('.categories_list');
+  /*search hover*/
+  const searchInput = document.querySelector('.search_input');
 
 
 
 
-/*active list category search*/
-btnListSearch.addEventListener('click',()=>{
-  btnListSearch.classList.toggle('active');
-});
-listCatSearch.addEventListener('click',(el)=>{
-  if(el.target.nodeName === "LI"){
-    activeCatSearch.innerText = el.target.innerText;
-  }
-})
+  /*active list category search*/
+  btnListSearch.addEventListener('click', () => {
+    btnListSearch.classList.toggle('active');
+  });
+  listCatSearch.addEventListener('click', (el) => {
+    if (el.target.nodeName === "LI") {
+      activeCatSearch.innerText = el.target.innerText;
+    }
+  })
 
-/*activecategory list */
-btnCategoryList.addEventListener('click',()=>{
-  btnCategoryList.classList.toggle('active');
-});
+  /*activecategory list */
+  btnCategoryList.addEventListener('click', () => {
+    btnCategoryList.classList.toggle('active');
+  });
 
-console.log('Work hard!');
+  /*search hover*/
+  searchInput.addEventListener('input', function() {
+    this.parentElement.classList.add('focus');
+  });
+  searchInput.addEventListener('blur', function() {
+    if (this.value.length < 1) {
+      this.parentElement.classList.remove('focus');
+    }
+  });
 
 })();
