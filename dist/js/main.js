a733ba630b905ab7050d3dbb7e206b0606a38fda
vitaliitsupylo <vitalii.tsupylo@yandex.ru>
2017-11-19 01:07:10 +0200

    comments

--- dist/js/main.js
+++ dist/js/main.js
@@ -77,44 +77,54 @@ module.exports = __webpack_require__(2);
 
 ;
 (() => {
-  'use strict';
-  /*Variables*/
+    'use strict';
+    /*Variables*/
 
-  /*active list category search*/
-  const btnListSearch = document.querySelector('.category_search');
-  const activeCatSearch = document.querySelector('.active_category_search');
-  const listCatSearch = document.querySelector('.list_category_search');
-  const btnCategoryList = document.querySelector('.categories_list');
-  /*search hover*/
-  const searchInput = document.querySelector('.search_input');
+    /*active list category search*/
+    const btnListSearch = document.querySelector('.category_search');
+    const activeCatSearch = document.querySelector('.active_category_search');
+    const listCatSearch = document.querySelector('.list_category_search');
+    const btnCategoryList = document.querySelector('.categories_list');
+    /*search hover*/
+    const searchInput = document.querySelector('.search_input');
+    /*user reviews*/
+    const reviewsCard = document.querySelectorAll('.user_reviews_card');
 
 
+    /*active list category search*/
+    btnListSearch.addEventListener('click', () => {
+        btnListSearch.classList.toggle('active');
+    });
+    listCatSearch.addEventListener('click', (el) => {
+        if (el.target.nodeName === "LI") {
+            activeCatSearch.innerText = el.target.innerText;
+        }
+    })
 
+    /*activecategory list */
+    btnCategoryList.addEventListener('click', () => {
+        btnCategoryList.classList.toggle('active');
+    });
 
-  /*active list category search*/
-  btnListSearch.addEventListener('click', () => {
-    btnListSearch.classList.toggle('active');
-  });
-  listCatSearch.addEventListener('click', (el) => {
-    if (el.target.nodeName === "LI") {
-      activeCatSearch.innerText = el.target.innerText;
+    /*search hover*/
+    searchInput.addEventListener('input', function () {
+        this.parentElement.classList.add('focus');
+    });
+    searchInput.addEventListener('blur', function () {
+        if (this.value.length < 1) {
+            this.parentElement.classList.remove('focus');
+        }
+    });
+    /*user reviews*/
+    if (reviewsCard.length > 0) {
+        for (let i = 0; i < reviewsCard.length; i++) {
+            let btn = reviewsCard[i].querySelector('.collapse');
+            btn.addEventListener('click', function () {
+                this.parentElement.classList.toggle('active');
+            })
+        }
     }
-  })
 
-  /*activecategory list */
-  btnCategoryList.addEventListener('click', () => {
-    btnCategoryList.classList.toggle('active');
-  });
-
-  /*search hover*/
-  searchInput.addEventListener('input', function() {
-    this.parentElement.classList.add('focus');
-  });
-  searchInput.addEventListener('blur', function() {
-    if (this.value.length < 1) {
-      this.parentElement.classList.remove('focus');
-    }
-  });
 
 })();
 
